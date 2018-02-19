b135d8055cf29a47162697f63446ccc30b496473
Amanda Ludwinski <Amanda.Ludwinski@icims.com>
2018-02-19 10:19:22 -0500

    Minor code cleanup in App.jsx.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-     <div>
+      <div>
         <h1>Hello World</h1>
-      </div>);
+      </div>
+    )
   }
 }
 
